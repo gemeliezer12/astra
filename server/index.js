1b2f08422ba69db9501e33b74dcff689e9158bb0
Gem Eliezer <gemeliezerespiritu@gmail.com>
2022-01-06 07:06:10 +0800

    update

--- server/index.js
+++ server/index.js
@@ -12,16 +12,7 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-const path = require("path")
-
-//This will create a middleware.
-//When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-// How to deploy a node-express and create-react-app on Heroku? The front-end and back-end are in separate folder and also have the git setup outside of those 2 folder. ! [remote rejected] main -> main (pre-receive hook declined) error: failed to push some refs to 'https://git.heroku.com/
-
 app.post("/payment", cors(), async (req) => {
-    // let {amount, id} = req.body
     const { email, payment_method } = req.body
 
     const customer = await stripe.customers.create({
