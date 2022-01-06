7115a37c4e3353092505152a5c13b4f8719c820d
Gem Eliezer <gemeliezerespiritu@gmail.com>
2022-01-06 11:57:33 +0800

    firestore

---
+++ server/firebase.js
@@ -0,0 +1,15 @@
+const firebase = require("firebase/compat/app")
+require("firebase/compat/firestore")
+require("firebase/compat/auth")
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDabmXRJ7rUXxnEuUErFNffOcl--2l5UpM",
+    authDomain: "fitness-and-health-c6be2.firebaseapp.com",
+    projectId: "fitness-and-health-c6be2",
+    storageBucket: "fitness-and-health-c6be2.appspot.com",
+    messagingSenderId: "461280175195",
+    appId: "1:461280175195:web:db6a70bc1a4a343c36cb7e",
+    measurementId: "${config.measurementId}"
+})
+
+export { firebase }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
+const firebase = require("firebase/compat/app")
+require("firebase/compat/firestore")
+require("firebase/compat/auth")
 require("dotenv").config()
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDabmXRJ7rUXxnEuUErFNffOcl--2l5UpM",
+    authDomain: "fitness-and-health-c6be2.firebaseapp.com",
+    projectId: "fitness-and-health-c6be2",
+    storageBucket: "fitness-and-health-c6be2.appspot.com",
+    messagingSenderId: "461280175195",
+    appId: "1:461280175195:web:db6a70bc1a4a343c36cb7e",
+    measurementId: "${config.measurementId}"
+})
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 const bodyParser = require("body-parser")
